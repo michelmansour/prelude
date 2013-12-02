a7509d60cedc6767e42b22a7e6ff2b0cc134a556
Michel Mansour <michel.mansour@gmail.com>
2013-12-02 18:26:12 -0500

    customizations of Prelude

---
+++ personal/init-local.el
@@ -0,0 +1,17 @@
+;;; init-local.el --- Personal customizations to Prelude
+
+;;; Commentary:
+
+;;; Code:
+
+;; Remove the scroll-/tool-/menu bars
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+;; Themes
+(disable-theme 'zen-burn)
+(load-theme 'solarized-dark t)
+
+(provide 'init-local)
+;;; init-local.el ends here
