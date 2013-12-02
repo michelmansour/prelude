9804f026adce5fe515385d8b2a7389f59cf1bda6
Michel Mansour <michel.mansour@gmail.com>
2013-12-02 18:26:12 -0500

    customizations of Prelude

--- personal/init-local.el
+++ personal/init-local.el
@@ -4,17 +4,12 @@
 
 ;;; Code:
 
-;; Keybindings
-(global-set-key "\C-w" 'backward-kill-word)
-
 ;; Remove the scroll-/tool-/menu bars
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 
 ;; Themes
-;(disable-theme 'zen-burn)
-;(load-theme 'solarized-dark t)
 
 (provide 'init-local)
 ;;; init-local.el ends here
